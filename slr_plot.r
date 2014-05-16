1e573bf0b3866dcdcea6712984494abb488a0b8f
Juan Sebastian Casallas <casallas@iastate.edu>
2014-05-16 18:52:46 -0500

    Regression plot for an slr fit with lm using ggplot2

---
+++ slr_plot.r
@@ -0,0 +1,32 @@
+library(ggplot2)
+#' Plot of a simple linear regression based on an lm fit using ggplot2
+#' @param fit an lm object
+#' @param post.sims number of simulations from the joint posterior to display (requires arm package).
+slr_plot.lm <- function(fit, post.sims=100){
+  coefs <- coef(fit)
+  coef_names <- names(coefs)
+  col_names <- names(fit$model)
+  # "sanitize" names giving defaults to avoid potential conflicts with ggplot2
+  names(fit$model) <- c("Y", "X")
+  
+  if(post.sims > 0 & require(arm)){
+	fit.sim <- sim(fit, post.sims)
+	sim.coef <- fit.sim@coef
+	sim.coef <- data.frame(sim.coef)
+	# Give default names to predictors
+	names(sim.coef) <- paste0("b", 0:(ncol(sim.coef)-1))
+  }else{
+    post.sims = 0
+  }
+  
+  p <- ggplot(aes(y=Y, x=X), data=fit$model)
+  if(post.sims > 0){
+	p <- p +
+	  geom_abline(aes(intercept=b0, slope=b1), data=sim.coef, alpha=0.075)
+  }
+  p + geom_abline(intercept=coefs[[1]], slope=coefs[[2]]) +
+	geom_point(colour="#e41a1c") +	
+	ylab(col_names[1]) +
+	xlab(col_names[2]) +
+	theme_bw()
+}

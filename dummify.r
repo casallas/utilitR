8ab6fc2b147860f7ba7752c13510890b52da6427
Juan Sebastian Casallas <casallas@iastate.edu>
2014-05-28 02:37:44 -0500

    dummify: Add functions to detect main and interaction effects

--- dummify.r
+++ dummify.r
@@ -129,3 +129,18 @@ bincode <- function(x, ref=NULL, sep=dummify.sep()){
   }
   data.frame(x.dum, check.names=F)
 }
+
+# Get the main effects of a given source in a matrix of dataframe
+mainef <- function(df, x, sep=dummify.sep(), sep.inter=dummify.sep_inter()){
+  if(require(stringr)){
+    str_detect(colnames(df), fixed(paste0(x, sep))) & !str_detect(colnames(df), fixed(sep.inter))
+  }
+}
+# Get the interaction effects of two sources in a matrix of dataframe
+intef <- function(df, x, y, sep=dummify.sep(), sep.inter=dummify.sep_inter()){
+  if(require(stringr)){
+    str_detect(colnames(df), fixed(paste0(x, sep))) &
+      str_detect(colnames(df), fixed(paste0(y, sep))) &
+      str_detect(colnames(df), fixed(sep.inter))
+  }
+}

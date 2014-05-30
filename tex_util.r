6b363fd7423b2ce953aaf031fc7593e47045e200
Juan Sebastian Casallas <casallas@iastate.edu>
2014-05-30 18:13:27 -0500

    tex_util: tex_num, tex_mean and tex_median
    - tex_num returns a number in tex format
    - tex_[mean|median] wrap tex_num to ease work with
    vectors

--- tex_util.r
+++ tex_util.r
@@ -1,4 +1,32 @@
-#' Print the summary of an mcmc in tex format
+#' Returns a number in tex format
+#'
+#' The number is rounded and optionally enclosed within "$"
+#'
+#' @param num a numeric object
+#' @param prefix Character string to prepend to the number estimate. For example "Mdn=". By default it's ""
+#' @param postfix Character string to append to the point estimate. For example "cm". By default it's ""
+#' @param math.mode if T, enclose output within "$"
+#' @digits digits number of decimal places to print
+tex_num <- function(num, prefix = "", postfix = "", math.mode = T, digits=texu_digits()){
+  num <- round(num, digits)
+  ans <- paste0("$", prefix, num, "$", postfix)
+  if(!math.mode) ans <- stringr::str_replace_all(ans, stringr::fixed("$"), "")
+  ans
+}
+
+#' Wrapper for tex_num that generates the mean of a vector
+#' By default the mean is preceded by "M=", which is the default APA style for means
+tex_mean <- function(vec, prefix = "M=", ...){
+  tex_num(mean(vec), prefix, ...)
+}
+
+#' Wrapper for tex_num that generates the median of a vector
+#' By default the median is preceded by "Mdn=", which is the default APA style for means
+tex_median <- function(vec, prefix = "Mdn=", ...){
+  tex_num(median(vec), prefix, ...)
+}
+
+#' Returns the summary of an mcmc in tex format
 #'
 #' The summary is printed in the following way "mu cred.mass*100\% HDI[hdi.lo, hdi.hi]"
 #' optionally encloses the output within "$" if math.mode == T
@@ -7,6 +35,7 @@
 #' @param mcmc a numeric vector containing the mcmc draws
 #' @param cred.mass a 0..1 number specifying the mass within the HDI
 #' @param est.str Character string to prepend to the point estimate. For example "Mdn=". By default it's ""
+#' @param units Character string to append to the point estimate. For example "cm". By default it's ""
 #' @param math.mode if T, enclose output within "$"
 #' @digits digits number of decimal places to print
 #'
@@ -19,9 +48,10 @@
 #' # The same without math mode
 #' tex_summary_mcmc(rnorm(1000), est.str="Mdn=", digits=3, math.mode = F)
 #' # [1] "Mdn=0.03, 95\\% HDI [-1.869, 1.983]"
-tex_summary_mcmc <- function(mcmc, cred.mass=0.95, est.str = "", math.mode = T, digits=texu_digits()){
+tex_summary_mcmc <- function(mcmc, cred.mass=0.95, est.str = "", units = "", math.mode = T, digits=texu_digits()){
   mcmc.sum <- lapply(summary_mcmc(mcmc), round, digits)
-  ans <- with(mcmc.sum, paste0("$", est.str, mu, "$, $", cred.mass*100, "\\%$ HDI $[", hdi.lo, ", ", hdi.hi, "]$"))
+  ans <- with(mcmc.sum, paste0("$", est.str, mu, "$", units,
+                               ", $", cred.mass*100, "\\%$ HDI $[", hdi.lo, ", ", hdi.hi, "]$"))
   if(!math.mode) ans <- stringr::str_replace_all(ans, stringr::fixed("$"), "")
   ans
 }

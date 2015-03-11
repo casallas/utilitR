417fd9f5aa42afb9bba3b3b5638a797c6923905d
Juan Sebastian Casallas <casallas@iastate.edu>
2015-03-11 15:02:38 -0500

    row_math: row_dot_prods

--- row_math.r
+++ row_math.r
@@ -6,3 +6,11 @@ row_norms <- function(vec_cols) sqrt(rowSums(vec_cols^2))
 #' @param p1_cols a matrix or data.frame containing numeric vectors in each row
 #' @param p2_cols a matrix or data.frame containing numeric vectors in each row
 row_distances <- function(p1_cols, p2_cols) row_norms(p1_cols - p2_cols)
+
+#' Gets the dot products between the rows of two matrices or data frames
+#' @param v1_cols a matrix or data.frame containing numeric vectors in each row
+#' @param v2_cols a matrix or data.frame containing numeric vectors in each row
+row_dot_prods <- function(v1_cols, v2_cols){
+  rowSums(v1_cols * v2_cols)
+  # Alternative implementation: diag(as.matrix(v1_cols) %*% t(v2_cols))
+}

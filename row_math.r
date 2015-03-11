c62f48f5dc20657e8ba9051d7819c3051d195283
Juan Sebastian Casallas <casallas@iastate.edu>
2015-03-11 16:10:10 -0500

    row_math: row unit vectors

--- row_math.r
+++ row_math.r
@@ -2,6 +2,10 @@
 #' @param vec_cols a matrix or data.frame containing numeric vectors in each row
 row_norms <- function(vec_cols) sqrt(rowSums(vec_cols^2))
 
+#' Gets the unit vector of each of the rows of a matrix or data frame
+#' @param vec_cols a matrix or data.frame containing numeric vectors in each row
+row_uvecs <- function(vec_cols) vec_cols/row_norms(vec_cols)
+
 #' Gets the geometric distances between the rows of two matrices or data frames
 #' @param p1_cols a matrix or data.frame containing numeric vectors in each row
 #' @param p2_cols a matrix or data.frame containing numeric vectors in each row

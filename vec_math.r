f85838a02bd7129e2855272842782a3e35157b1d
Juan Sebastian Casallas <casallas@iastate.edu>
2015-03-31 16:37:00 -0500

    vec_math: square norm

--- vec_math.r
+++ vec_math.r
@@ -1,6 +1,9 @@
 #' Calculates the norm of a vector
 vec_norm <- function(vec) sqrt(sum(vec^2))
 
+#' Calculates the square norm of a vector
+vec_norm2 <- function(vec) sum(vec^2)
+
 #' Calculates the distance between two points
 vec_distance <- function(p1, p2) vec_norm(p1 - p2)
 
